3a0e0969572f28c31b9100d9e399a2065cbda315
Hok S. Uy <hoksuy@gmail.com>
2021-09-06 15:07:31 -0700

    added voters table schema and seeds

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/api/candidates', (req, res) => {
                  ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => {
-        if(err) {
+        if (err) {
             res.status(500).json({ error: err.message });
             return;
         }
@@ -61,7 +61,7 @@ app.get('/api/candidate/:id', (req, res) => {
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
-        if(err) {
+        if (err) {
             res.status(400).json({ error: err.message });
             return;
         }
@@ -77,9 +77,9 @@ app.delete('/api/candidate/:id', (req, res) => {
     const params = [req.params.id];
 
     db.query(sql, params, (err, result) => {
-        if(err)
-            res.statusMessage(400).json({ error: err.message});
-        else if(!result.affectedRows) {
+        if (err)
+            res.statusMessage(400).json({ error: err.message });
+        else if (!result.affectedRows) {
             res.json({
                 message: 'Candidate not found'
             });
@@ -93,9 +93,37 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
 });
 
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 app.post('/api/candidate', ({ body }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if(errors) {
+    if (errors) {
         res.status(400).json({ error: errors });
         return;
     }
@@ -104,7 +132,7 @@ app.post('/api/candidate', ({ body }, res) => {
     const params = [body.first_name, body.last_name, body.industry_connected];
 
     db.query(sql, params, (err, result) => {
-        if(err) {
+        if (err) {
             res.status(400).json({ error: err.message });
             return;
         }
@@ -112,6 +140,56 @@ app.post('/api/candidate', ({ body }, res) => {
             message: 'success',
             data: body
         });
+    });
+});
+
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            // checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
     });
 });
 
