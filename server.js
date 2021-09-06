c532c14bb66fe1f81039ce247f347fc61699c9d1
Hok S. Uy <hoksuy@gmail.com>
2021-09-05 21:14:41 -0700

    created express app for sql queries

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '7364',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
+
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if(err)
+        console.log(err);
+    console.log(result);
+});
+
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if(err)
+        console.log(err);
+    console.log(row);
+});
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err)
+//         console.log(err);
+//     console.log(result);
+// });
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
